6f5ed32fc4ad4d408e4293ae75028d85a27ff009
Vishal Chandra <vishalchandra724@gmail.com>
2020-02-08 12:50:37 -0500

    implement rotation control using color sensor as an encoder

---
+++ src/main/java/frc/robot/commands/colorwheel/RotationControl.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.colorwheel;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.*;
+
+public class RotationControl extends CommandBase {
+
+  ColorWheel colorWheel;
+  char initColor;
+  int colorPasses = 0;
+
+  /**
+   * Add Requirements
+   */
+  public RotationControl(ColorWheel colorWheelSystem) {
+    colorWheel = colorWheelSystem;
+    addRequirements(colorWheel);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    initColor = colorWheel.readColor();
+    colorWheel.startMotor(); 
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    char currentColor = colorWheel.readColor();
+    if(currentColor == initColor) colorPasses++;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    colorWheel.stopMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(colorPasses == 6)
+      return true;
+    else
+      return false;
+  }
+}
