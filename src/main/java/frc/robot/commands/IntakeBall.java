7f97193f7683c09f6fb76a7700159f036ae145df
Vishal Chandra <vishalchandra724@gmail.com>
2020-02-25 04:09:32 -0500

    intake command

--- src/main/java/frc/robot/commands/IntakeBall.java
+++ src/main/java/frc/robot/commands/IntakeBall.java
@@ -18,24 +18,18 @@ public class IntakeBall extends CommandBase {
     addRequirements(intake);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    intake.runIntake();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    intake.stopIntake();
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return intake.ballInRobot();
   }
 }
