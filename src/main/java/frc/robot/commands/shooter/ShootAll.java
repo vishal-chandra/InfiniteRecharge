8350cf5311866e3fe8331e636b20869feae15009
Vishal Chandra <vishalchandra724@gmail.com>
2021-02-24 15:10:46 -0500

    move shooter commands to shooter folder

--- src/main/java/frc/robot/commands/shooter/ShootAll.java
+++ src/main/java/frc/robot/commands/shooter/ShootAll.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.intake;
+package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
