519bf13c6c7e3d23182ef66058844d4b5a7bfe5b
Steve Li <lithafnium2000@gmail.com>
2021-03-16 16:52:16 -0400

    recalculate kF and PID

--- src/main/java/frc/robot/commands/shooter/ShootAll.java
+++ src/main/java/frc/robot/commands/shooter/ShootAll.java
@@ -17,6 +17,7 @@ public class ShootAll extends SequentialCommandGroup {
       new WaitCommand(0.25),
       new Index(intake),
       new ShootBall(shooter, intake, power),
+      new WaitCommand(0.25),
       new InstantCommand(() -> shooter.stopFlywheels(), shooter),
       new InstantCommand(() -> intake.stopIntake(), intake),
       new InstantCommand(() -> intake.stopTower(), intake)
